1ee6e674daeefc081498f6166c63332b1feeabfa
Rozz <parveenrawal73@gmail.com>
2025-08-07 16:15:31 +0530

    routing  and global color setup complete

---
+++ src/pages/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h1 className='text-lightBtn text-5xl font-bold'>Hlo from test routes is working 😊</h1>
+        </div>
+    );
+}
+
+export default Test;
