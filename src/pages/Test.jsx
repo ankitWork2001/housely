08e74951fac8b9ffda53b6e6c63510ceb1615eac
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2025-08-12 18:42:45 +0530

    added home-page

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Navbar from '../component/Navbar';
-import Footer from '../component/Footer';
+import React from "react";
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
+import Home from "../component/Home";
 
 const Test = () => {
-    return (
-
-        <div>
-            <Navbar/>
-            <h1 className='text-lightBtn text-5xl font-bold'>Hlo from test routes is working 😊</h1>
-            <Footer/>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+  );
+};
 
 export default Test;
